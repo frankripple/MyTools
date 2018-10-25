57bbe02acecec48304e63a8c94d73b7e291bb803
Junshi Cui <frankripple@163.com>
2018-10-25 12:20:58 +0800

    findaddfilesbyCondition
    findaddfilesbyCondition

--- testcase.py
+++ testcase.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-  
 import tools
+import re
 
 current_path = 'log/'
-r = tools.findaddfiles(current_path,'show run')
+#r = tools.findaddfilesbyname(current_path,r'show run.txt')
+
+def findrun(filename):
+    if re.search(r'show run.txt',filename):
+        return True
+    elif re.search(r'show running-config.txt',filename):
+        return True
+    else:
+        return False
+
+r = tools.findaddfilesbyCondition(current_path,findrun)
 print(r)
+print(len(r))
+
+def findcommand(filename):
+    f = open(filename,'r')
+    content = f.read()
+    f.close()
+    if re.search('hostname ',content):
+        return True
+    else:
+        return False
+
+#r = tools.findaddfilesbyCondition(current_path,findcommand)
--- tools.py
+++ tools.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-  
 import os
 import re
-def findaddfiles(rootdir,pfilename):
+
+
+def findaddfilesbyCondition(rootdir,Condition):
+    '''
+    rootdir is the root path for the file search.
+    Condition is a function for match the file. And Condtion must accept a parameter file_path and return True or False
+    return value is the list of all the fullname of files.
+    '''
+    result = list()
+    for parent, dirnames, filenames in os.walk(rootdir,followlinks=True):
+        for filename in filenames:
+            file_path = os.path.join(parent, filename)
+            if Condition(file_path):
+                result.append(file_path)
+    return result
+
+def findaddfilesbyname(rootdir,pfilename):
+    '''
+    rootdir is the root path for the file search.
+    pfilename is the parttern for match the filename
+    return value is the list of all the fullname of files.
+    '''
     result = list()
     for parent, dirnames, filenames in os.walk(rootdir,followlinks=True):
         for filename in filenames:
