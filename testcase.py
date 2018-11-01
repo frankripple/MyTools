3689b1649cf8adab47397deddaeee629bf5fb23a
Junshi Cui <frankripple@163.com>
2018-11-01 14:22:36 +0800

    Divide function code and write

--- testcase.py
+++ testcase.py
@@ -45,53 +45,57 @@ if __name__ == "__main__":
     #copy_configuration(current_path)
     r = tools.findaddfilesbyname('Running-configuration\\')
     versions = list()
+    ACLs = list()
+    ACLs.append(('Device',"IP"))
+    for p in r:
+        hostname = os.path.basename(p).split('.')[0]
+        f = open(p,'r')
+        content = f.readlines()
+        f.close()
+        device_type = ''
+        ACLs.append((hostname,""))
+        count = 0
+        start = 0
+        for l in content:
+            if device_type == '':
+                t = re.search('ersion (.*?)\n',l)
+                if t:
+                    device_type = t.groups()[0].strip()
+                    if (device_type in higher_version) or (device_type in lower_version) or (device_type in firewall_version):
+                        #print('%s version is matched. Version is %s' %(hostname,device_type) )
+                        versions.append((hostname,device_type))
+                    else:
+                        pass
+                        #print('%s version is not matched. Version is %s' %(hostname,device_type) )
+            
+            if device_type in higher_version:
+                t = re.search('access-list 89 permit (.*?)\n',l)
+                if t:
+                    ACLs.append((hostname,t.groups()[0]))
+                    count +=1
+
+            if device_type in lower_version:
+                if start == 0:
+                    t = re.search('ip access-list (\d+)',l)
+                    if t and t.groups()[0].strip() == '89':
+                        start = 1
+                else:
+                    t = re.search('permit \S+ (.*?) any',l)
+                    if t:
+                        ACLs.append((hostname,t.groups()[0]))
+                        count +=1
+
+            if device_type in firewall_version:
+                t = re.search('ssh ([0-9.]+ [0-9.]+)',l)
+                if t:
+                    ACLs.append((hostname,t.groups()[0]))
+                    count +=1
+        if count == 0:
+            print(hostname)
+
     with open('ACLs.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(('Device',"IP"))
-        for p in r:
-            hostname = os.path.basename(p).split('.')[0]
-            f = open(p,'r')
-            content = f.readlines()
-            f.close()
-            device_type = ''
-            writer.writerow((hostname,""))
-            count = 0
-            start = 0
-            for l in content:
-                if device_type == '':
-                    t = re.search('ersion (.*?)\n',l)
-                    if t:
-                        device_type = t.groups()[0].strip()
-                        if (device_type in higher_version) or (device_type in lower_version) or (device_type in firewall_version):
-                            #print('%s version is matched. Version is %s' %(hostname,device_type) )
-                            versions.append((hostname,device_type))
-                        else:
-                            #print('%s version is not matched. Version is %s' %(hostname,device_type) )
-                
-                if device_type in higher_version:
-                    t = re.search('access-list 89 permit (.*?)\n',l)
-                    if t:
-                        writer.writerow((hostname,t.groups()[0]))
-                        count +=1
-
-                if device_type in lower_version:
-                    if start == 0:
-                        t = re.search('ip access-list (\d+)',l)
-                        if t and t.groups()[0].strip() == '89':
-                            start = 1
-                    else:
-                        t = re.search('permit \S+ (.*?) any',l)
-                        if t:
-                            writer.writerow((hostname,t.groups()[0]))
-                            count +=1
-
-                if device_type in firewall_version:
-                    t = re.search('ssh ([0-9.]+ [0-9.]+)',l)
-                    if t:
-                        writer.writerow((hostname,t.groups()[0]))
-                        count +=1
-            if count == 0:
-                print(hostname)
+        writer.writerows(ACLs)
 
     with open('version.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
